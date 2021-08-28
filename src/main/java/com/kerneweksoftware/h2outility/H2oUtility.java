a6021f25a05d4ce9c8c3b3d1591545943dc54b31
VesnaBrucoms <etstevens60@gmail.com>
2021-08-28 19:49:28 +0100

    H2OUtility will ask for archive path if none provided in the args
    * Corrected target Java version to 16

--- src/main/java/com/kerneweksoftware/h2outility/H2oUtility.java
+++ src/main/java/com/kerneweksoftware/h2outility/H2oUtility.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.zip.DataFormatException;
 
 import com.kerneweksoftware.h2outility.exceptions.IncorrectFileTypeException;
@@ -15,10 +16,18 @@ import com.kerneweksoftware.h2outility.services.DirectoryOutput;
 public class H2oUtility 
 {
 
-    public static void main(String[] args) throws IOException, DataFormatException, IncorrectFileTypeException {
-        byte[] rawArchive = Files.readAllBytes(Paths.get("C:\\Users\\etste\\Documents\\Projects\\h2o-utility\\archives\\Text.H2O"));
-        ByteBuffer archive = ByteBuffer.wrap(rawArchive);
-        archive.order(ByteOrder.LITTLE_ENDIAN);
+    public static void main(String[] args) throws IOException, DataFormatException {
+        String archivePath = "";
+        String archiveName = "";
+        if (args.length == 0) {
+            archivePath = getUserInput();
+            archiveName = getArchiveName(archivePath);
+        } else {
+            archivePath = args[args.length - 1];
+            archiveName = getArchiveName(archivePath);
+        }
+
+        ByteBuffer archive = readArchive(archivePath);
 
         ArchivedData archiveData = null;
         try {
@@ -28,9 +37,40 @@ public class H2oUtility
             System.out.println(e.getMessage());
         }
 
-        archiveData.setName("Text");
+        archiveData.setName(archiveName.replace(".H2O", ""));
 
         DirectoryOutput output = new DirectoryOutput(archiveData);
         output.write();
+    }
+
+    protected static String getUserInput() {
+        String path = "";
+        System.out.println("Please provide the path of the archive to unpack");
+        System.out.print("-> ");
+        Scanner sc = new Scanner(System.in);
+        try {
+            path = sc.next();
+        } finally {
+            sc.close();
+        }
+        return path;
+    }
+
+    protected static String getArchiveName(String path) {
+        var name = "";
+        if (path.contains("\\")) {
+            String[] splitPath = path.split("\\\\");
+            name = splitPath[splitPath.length - 1];
+        } else {
+            name = path;
+        }
+        return name;
+    }
+
+    protected static ByteBuffer readArchive(String path) throws IOException {
+        byte[] rawArchive = Files.readAllBytes(Paths.get(path));
+        ByteBuffer archive = ByteBuffer.wrap(rawArchive);
+        archive.order(ByteOrder.LITTLE_ENDIAN);
+        return archive;
     }
 }
