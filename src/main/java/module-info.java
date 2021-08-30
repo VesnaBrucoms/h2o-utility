eb69e851dfd455e4ed2844049b06a39b44dad541
VesnaBrucoms <etstevens60@gmail.com>
2021-08-30 15:24:32 +0100

    First attempt to modularise

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.kerneweksoftware.h2outility {
+    requires dbc.reader;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires static lombok;
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,10 @@
+open module com.kerneweksoftware.h2outility {
+    requires dbc.reader;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires static lombok;
+
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.engine;
+    requires org.mockito;
+}
